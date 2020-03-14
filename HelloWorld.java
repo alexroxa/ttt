15c686c74dc0e2a4202c59f65695725bccfadb94
qwer <montague202017@gmail.com>
2020-03-14 16:20:31 +0800

    1231

---
+++ HelloWorld.java
@@ -0,0 +1,38 @@
+//package servlet_examples;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloWorld extends HttpServlet {
+	
+	int count = 0;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			                         throws ServletException, IOException {
+		doPost(req, res);
+
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			                          throws ServletException, IOException {
+			                          	
+		res.setHeader("Cache-Control","no-store"); //HTTP 1.1
+		res.setHeader("Pragma","no-cache");        //HTTP 1.0
+		res.setDateHeader("Expires",0);                        	
+
+		res.setContentType("text/html; charset=Big5");
+		PrintWriter out = res.getWriter();
+		
+		int size = res.getBufferSize(); // returns 8192 or greater
+
+    // Record the default size, in the log
+    log("The default buffer size is " + size);
+
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>Hello World</TITLE></HEAD>");
+		out.println("<BODY>");
+		out.println("<BIG>Hello World , 世界你好 !</BIG>="+(++count));
+		out.println("</BODY></HTML>");
+	}
+}
